d5148501a1abe0da6629877955714136d3a9f390
Dev-Sam99 <developers.sam99@gmail.com>
2024-01-05 15:44:56 +0530

    asyncHandler apiRes apiErr cors cookieParser added

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,17 @@
 import dotenv from "dotenv";
-
 import connectDB from "./db/index.js";
-
+import express from "express";
+const app = express();
 dotenv.config({
     path:'./env'
 })
-
-connectDB();
+const port = process.env.PORT || 5000;
+connectDB()
+.then(()=>{
+    app.listen(port,()=>{
+        console.log(`app is running on ${port}`);
+    })
+})
+.catch((err)=>{
+    console.log('MONGO DB connection failed...!',err)
+});
