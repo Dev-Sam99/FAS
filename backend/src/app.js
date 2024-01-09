9bfa612bbfbf146b8f24f842c4a3ed9b9f1d2a84
Dev-Sam99 <developers.sam99@gmail.com>
2024-01-09 12:58:01 +0530

    basic route for register user added

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
-
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
     credentials : true
@@ -13,6 +12,9 @@ app.use(express.urlencoded({extended: true, limit:"16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+import userRouter from "./routes/user.routes.js";
 
-
-export{app};
+//routes declarations
+app.use("/api/v1/users",userRouter);
+export {app};
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-import express from "express";
-const app = express();
+import {app} from "./app.js";
+
 dotenv.config({
     path:'./env'
 })
+
 const port = process.env.PORT || 5000;
 connectDB()
 .then(()=>{
